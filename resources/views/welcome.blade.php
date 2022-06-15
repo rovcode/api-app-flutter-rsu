80a7c8d26660d45d50cdfdd8a2fa42db859851ba
Rony Velásquez <velasquezcordovar@gmail.com>
2022-06-14 23:32:38 -0500

    Cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,55 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <title>Api-App-Flutter</title>
 </head>
 <body>
-   <h1 style="text-align: center">API RESU V1</h1>
+   <div class="container mt-5">
+     <h1 class="alert alert-warning" style="text-align: center">API RSU - EQUIPO 10 - Versión 1.0</h1>
+      <div class="container">
+         <h4>Documentación</h4>
+          <ul>
+            <li class="text-success">
+              point: http://api-rsu.herokuapp.com/api/usuarios
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+              <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+              <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+            </svg>
+          </li>
+            <li class="text-success">
+              point: http://api-rsu.herokuapp.com/api/roles
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+                <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+              </svg>
+            </li>
+            <li>point: http://api-rsu.herokuapp.com/api/platos</li>
+            <li>point: http://api-rsu.herokuapp.com/api/carta</li>
+            <li>point: http://api-rsu.herokuapp.com/api/reservas</li>
+            <li>point: http://api-rsu.herokuapp.com/api/ventas</li>
+            <li>point: http://api-rsu.herokuapp.com/api/calificaciones</li>
+            <li>point: http://api-rsu.herokuapp.com/api/mesas</li>
+            <li>point: http://api-rsu.herokuapp.com/api/promociones</li>
+            <li>point: http://api-rsu.herokuapp.com/api/aforo</li>
+            <li>point: http://api-rsu.herokuapp.com/api/menus</li>
+          </ul>
+
+         <div class="row mt-5">
+          <div class="col-sm-4">
+             Creado by : <a href="https://www.ronyvelasquez.com/" target="_blank">Rony Velásquez</a>
+          </div>
+          <div class="col-sm-4">
+            <a href="https://github.com/rovcode" target="_blank">GitHub</a>
+          </div>
+          <div class="col-sm-4">
+            <a href="">LinkedIn</a>
+          </div>
+         </div>
+      </div>
+   </div>
+   <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
